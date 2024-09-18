a2a93078da7c851e2d32f6b27ac84eba1ab93cab
test1 <31kiel12@gmail.com>
2024-09-18 13:46:33 +0300

    Добавьте файлы проекта.

---
+++ Loops4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int cells = 1;
+
+        for (int hours = 3; hours <= 24; hours += 3)
+        {
+            cells *= 2;
+            Console.WriteLine($"Через {hours} часов будет {cells} клеток.");
+        }
+    }
+}
